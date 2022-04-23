d160d964f2a1c2963928a945778acd35789df2e0
Trần Viết Hoàng <81605215+tranviethoangsmile@users.noreply.github.com>
2022-04-23 15:40:53 +0700

    demo swagger

--- app/router/route.js
+++ app/router/route.js
@@ -1,28 +1,54 @@
-const ProductConller = require('../controller/productController');
-const TransactionController = require('../controller/transactionController');
-const redis = require('redis');
+const ProductConller = require("../controller/productController");
+const TransactionController = require("../controller/transactionController");
+const redis = require("redis");
 const REDIS_PORT = process.env.REDIS_PORT || 5000;
 const client = redis.createClient(REDIS_PORT);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *      product:
+ *        type: object
+ *        required:
+ *        properties:
+ *              id:
+ *                  type: interger
+ *                  description: the auto generated id
+ *              name:
+ *                  type: string
+ *                  description: max length is 255 characters
+ *              price:
+ *                  type: float
+ *                  description: not null
+ *        example:
+ *         name: iphone 11
+ *         price: 1000
+ *
+ */
 routes = (app) => {
-    app.get('/', (req, res) => {
-        res.render('system');
-    });
-    // get all product 
-    app.get('/api/product/getall', ProductConller.findAll);
+    // app.get('/', (req, res) => {
+    //     res.render('system');
+    // });
+    // get all product
+    app.get("/api/product/getall", ProductConller.findAll);
     // get product by id
-    app.get('/api/product/:tagId', ProductConller.cache, ProductConller.findOne);
+    app.get("/api/product/:tagId", ProductConller.cache, ProductConller.findOne);
     // create new product
-    app.post('/api/product/create', ProductConller.create);
+    app.post("/api/product/create", ProductConller.create);
     // update produce
-    app.patch('/api/product/update', ProductConller.update);
+    app.patch("/api/product/update", ProductConller.update);
     // delete product
-    app.get('/api/product/delete/:tagId', ProductConller.delete);
+    app.get("/api/product/delete/:tagId", ProductConller.delete);
     // search product
-    app.post('/api/product/search', ProductConller.findbyname);
-    // create bill 
-    app.post('/api/transaction/create', TransactionController.create)
-        // get list of user
-    app.get('/api/transaction/:tagId', TransactionController.cache, TransactionController.findByIdUser);
-}
+    app.post("/api/product/search", ProductConller.findbyname);
+    // create bill
+    app.post("/api/transaction/create", TransactionController.create);
+    // get list of user
+    app.get(
+        "/api/transaction/:tagId",
+        TransactionController.cache,
+        TransactionController.findByIdUser
+    );
+};
 
 module.exports = routes;
--- index.js
+++ index.js
@@ -4,10 +4,30 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 var path = require("path");
 const app = express();
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const routes = require("./app/router/route");
+const option = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "test api",
+            version: "1.0.0",
+            description: "test api with swagger"
+        },
+        servers: [{
+            url: "http://localhost:3000"
+        }],
+    },
+    apis: ['./app/router/*.js']
+}
+
+const specs = swaggerJsDoc(option);
+app.use("/", swaggerUI.serve, swaggerUI.setup(specs));
+
 app.use(bodyParser());
 app.set('view engine', 'ejs');
 app.set("views", path.resolve(__dirname, "./app/views/"));
-const routes = require("./app/router/route");
 const port = process.env.PORT || 3000;
 routes(app);
 app.use(express.json());
